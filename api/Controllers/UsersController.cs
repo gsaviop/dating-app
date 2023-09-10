6e137fe2d0f6b7518e1bbcf897aac314a9749de4
Gabriel S. Pacheco <gabrielsaviolip@gmail.com>
2023-09-10 14:13:43 -0300

    initial setup for the datingApp API using ASPNET and C#

---
+++ api/Controllers/UsersController.cs
@@ -0,0 +1,31 @@
+﻿using api.Data;
+using api.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class UsersController : ControllerBase
+{
+    private readonly DataContext _context;
+
+    public UsersController(DataContext context) 
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<AppUser>>> GetUsersList()
+    {
+        return await _context.Users.ToListAsync();
+    }
+
+    [HttpGet("{id}")] //api/users/[idNum]
+    public async Task<ActionResult<AppUser>> getUser(int id)
+    {
+        return await _context.Users.FindAsync(id);
+    }
+
+}
